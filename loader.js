6a719d10796ec908c6da950679dbf38275e91efe
RogerArenas <rogerioarenas@hotmail.com>
2017-08-06 03:43:18 -0300

    Add files via upload

--- loader.js
+++ loader.js
@@ -1,19 +1,3 @@
-const port = 3003
-
-const bosyParser = require ('body-parser')
-const express = require('express')
-const server = express()
-
-
-server.user(bodyParser.urlencoded({extended: true}))
-server.user(bodyParser.json())
-
-server.listen(port, function() {
-   console.log('BACKEND is running on port ${port}.')
-})
-
-
- server.use(function(req, res, next){
-
-   res.send('Funcionou')
- })
+const server = require('./config/Server')
+require('./config/database')
+require('./config/routes')(server)
